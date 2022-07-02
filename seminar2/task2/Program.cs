6f2505be131b18b569f3cb170b3ce0826bc35ea4
Natasha <lickevich.natasha@rambler.ru>
2022-07-02 22:52:17 +0300

    решение задачи 2

---
+++ seminar2/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// int num = new Random().Next(10, 999);
+Console.WriteLine("Введите число:");
+string numbers = Console.ReadLine();   // прям здесь считываем цифры со ввода
+
+string thirdNumber () {
+
+    if (numbers.Length <= 2) {          // если длина символов в строке меньше 2
+        return "В числе нет третьей цифры!";
+    }
+    else {
+        return "Третья цифра в числе " + numbers+ " : " + numbers[2];  // выведи символ с индексом 2
+    }
+}
+
+Console.WriteLine(thirdNumber());
